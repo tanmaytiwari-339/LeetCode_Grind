67477d14eedc0e47792b050cc328754cad9caa04
tanmaytiwari-339 <2k24.csaiml1e.2411349@gmail.com>
2026-09-08 06:54:46 +0530

    Time: 2 ms (91.78%), Space: 47.9 MB (20.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,4 +1,6 @@
 class Solution {
+    //Approach 1 and 2( TC = O(n^2))
+    /*
     public void moveZeroes(int[] nums) {
         int n = nums.length;
         for(int i=n-1;i>=0;i--){
@@ -9,12 +11,34 @@ class Solution {
     }
     public void arrange(int[] nums,int i){
         int n=nums.length;
-        while(i<n-1 && i+1!=0){
+        while(i<n-1 && nums[i+1]!=0){
             int temp = nums[i];
             nums[i]=nums[i+1];
             nums[i+1]=temp;
             i++;
         }
+    }
+    */
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int i=0;
+        int j=0;
+        while(j<n){
+            if(nums[i]!=0){
+                i++;
+            }else{
+                if(nums[j]!=0){
+                    swap(nums,i,j);
+                    i++;
+                }
+            }
+            j++;
+        }
+    }
+    public void swap(int[] nums,int i,int j){
+        int temp= nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     
 }
