f699364c93c42098d4693a672e835764d048bb83
tanmaytiwari-339 <2k24.csaiml1e.2411349@gmail.com>
2026-09-22 19:15:31 +0530

    Time: 1 ms (99.88%), Space: 63.1 MB (6.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,8 @@
 class Solution {
+    // Brute force Approach(TC => O(n+k) SC = > O(n))
+    /*
     public int majorityElement(int[] nums) {
+
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i : nums){
             map.put(i,map.getOrDefault(i,0)+1);
@@ -11,6 +14,34 @@ class Solution {
             }
         }
         return 0;
-        
+    }
+    */
+    // Removed the redundency of creatink full map TC => O(n) SC => O(n)
+    /*
+    public int majorityElement(int[] nums) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i : nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+            if(map.get(i) > n/2){
+                return i;
+            }
+        }
+        return 0;
+    }
+    */
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int ele = 0;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                ele=nums[i];
+                count=1;
+            }else if(nums[i]==ele) count++;
+            else count--;
+        }
+        return ele;
     }
 }
